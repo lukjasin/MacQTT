a9ce6c966c31c99b27a9f1f970e64bf1a1c2ca8f
lukjasin <luk.jasin@pm.me>
2026-03-30 22:09:40 +0200

    Initial Commit

---
+++ MacQTT/MacQTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacQTTApp.swift
+//  MacQTT
+//
+//  Created by Łukasz Jasiński on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacQTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
